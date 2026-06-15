db80e4d502f8fdbf0ff36faa6b839673ed9cc195
Artem Chornenkiy <lucaskenzov1@gmail.com>
2026-06-15 14:19:45 +0300

    Новий опис коміту

---
+++ hm1.py
@@ -0,0 +1,5 @@
+number1 =int(input('Введіть перше число'))
+number2 = int(input('Введіть друге число'))
+number3 = int(input('Введіть третє число'))
+average = (number1 + number2 + number3)/3
+print ('cереднє арифметичне трьох чисел =', average)
